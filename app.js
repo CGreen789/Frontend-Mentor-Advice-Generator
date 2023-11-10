661952654f2b18bfb73df13f62e0fe74880ee08c
Catherine Green <catherine.green@iceland.co.uk>
2023-11-10 12:25:09 +0000

    improved widths, dynamically showing images depending on screen sizes, media query

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 // Fetching data from the API: https://api.adviceslip.com/
 
 const getAdvice = () => {
-const apiURL = 'https://api.adviceslip.com/advice'
+  const apiURL = "https://api.adviceslip.com/advice";
 
-fetch(apiURL)
-.then(response => response.json())
-.then(data => {
-    const advice = data.slip.advice
-    const adviceID = data.slip.id
-    console.log(advice, adviceID)
-    const adviceIDPlaceholder = document.querySelector("#advice-id")
-    const advicePlaceholder = document.querySelector("#advice")
-    advicePlaceholder.innerText = advice
-    adviceIDPlaceholder.innerText = `ADVICE #${adviceID}`
-})
-}
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((data) => {
+      const advice = data.slip.advice;
+      const adviceID = data.slip.id;
+      console.log(advice, adviceID);
+      const adviceIDPlaceholder = document.querySelector("#advice-id");
+      const advicePlaceholder = document.querySelector("#advice");
+      advicePlaceholder.innerText = `“${advice}”`;
+      adviceIDPlaceholder.innerText = `ADVICE #${adviceID}`;
+    });
+};
 // Fetch a new API call on button click
-const diceButton = document.querySelector("#dice-button")
-diceButton.addEventListener('click', () => getAdvice())
+const diceButton = document.querySelector("#dice-button");
+diceButton.addEventListener("click", () => getAdvice());
 
+window.addEventListener("DOMContentLoaded", function () {
+  const patternDivider = document.getElementById("pattern-divider");
 
+  function updateImage() {
+    if (window.innerWidth > 768) {
+      patternDivider.src = "./images/pattern-divider-desktop.svg";
+    } else {
+      patternDivider.src = "./images/pattern-divider-mobile.svg";
+    }
+  }
+
+  // Initial update on page load
+  updateImage();
+
+  // Update on window resize
+  window.addEventListener("resize", updateImage);
+});
 
 getAdvice();
